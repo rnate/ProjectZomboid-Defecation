85449cc59b69211a2d127b912570d62a7afd04e2
rnate <nathanh2005@yahoo.com>
2022-03-31 14:23:25 -0500

    Delete lua directory

--- lua/server/camping/fuel.lua
+++
@@ -1,8 +0,0 @@
-require "Camping/camping_fuel";
-
-campingFuelType.humanFeces = 0.25
-campingFuelType["humanFeces"] = 0.25
-campingFuelType.PooPants = 0.25
-campingFuelType["PooPants"] = 0.25
-campingFuelType.PooSkirt = 0.25
-campingFuelType["PooSkirt"] = 0.25
